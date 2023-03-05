7afacac3feef466f3f6e74723f0e5a76e22ef1d2
KimiyaBr <125910604+KimiyaBr@users.noreply.github.com>
2023-03-05 20:50:35 +0330

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+main() {
+	int A[3][3], i, j, answer = 0;
+	for(i=0;i<=2;i++)
+	{
+		for (j = 0;j<=2;j++)
+		{
+			cout << "enter number" << i << j<< endl;
+			cin>>A[i][j]
+		}
+	}
+	answer = A[0][0] * ((A[1][1] * A[2][2]) - (A[1][2] * A[2][1])) - A[1][0] * ((A[0][1] * A[2][2]) - (A[0][2] * A[2][1])) + A[0][2] * ((A[0][1] * A[1][2]) - (A[0][2] * A[1][1]));
+	cout << "answer: " << answer << endl;
+
+}
+
